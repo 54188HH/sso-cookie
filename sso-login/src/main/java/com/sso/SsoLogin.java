3eab73e714e58410c872c401c799cc022d9c3ff7
54188HH <l7492541>
2020-06-14 19:20:09 +0800

    "sso单点登录"

---
+++ sso-login/src/main/java/com/sso/SsoLogin.java
@@ -0,0 +1,11 @@
+package com.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoLogin {
+  public static void main(String[] args) {
+      SpringApplication.run(SsoLogin.class,args);
+  }
+}
---
+++ sso-login/src/main/java/com/sso/pojo/User.java
@@ -0,0 +1,16 @@
+package com.sso.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class User {
+    private String username;
+    private Integer id;
+    private String password;
+}

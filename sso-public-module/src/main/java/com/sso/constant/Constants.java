3d222accf049cca8131c34270184bd106cb9d9c0
lzq <15903031938@163.com>
2020-06-17 17:48:51 +0800

    "公共代码模块"

--- sso-public-module/src/main/java/com/sso/constant/Constants.java
+++ sso-public-module/src/main/java/com/sso/constant/Constants.java
@@ -7,5 +7,6 @@ package com.sso.constant;
  * @create: 2020-06-17 16:08
  **/
 public class Constants {
+    //通过token获取用户信息
     public final static String LOGIN_INFO_URL = "http://login.ssologin.com:9000/info?token=";
 }
